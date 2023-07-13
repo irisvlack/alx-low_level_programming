8919ec45d518a73fe88633c9d706dd5077b678a0
irisvlack <ivynjoroge30@gmail.com>
2023-07-13 14:17:07 -0700

    2

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -12,39 +12,38 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *a;
+	char *a;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Holberton");
+	strcpy(a + 9, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	return (0);
 }
