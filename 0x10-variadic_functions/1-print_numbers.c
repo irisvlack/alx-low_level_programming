03e199c1c40dc441b38793b359f496027c9550c6
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-07-20 19:51:10 +0300

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,15 +1,20 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
-void print_numbers(const char *separator, const unsigned int n, ...) {
+void print_numbers(const char *separator, const unsigned int n, ...) 
+{
     va_list args;
+    
     va_start(args, n);
-    for (unsigned int i = 0; i < n; i++) {
-        if (i != 0 && separator != NULL) {
+    for (unsigned int i = 0; i < n; i++) 
+    {
+        if (i != 0 && separator != NULL)
+        {
             printf(" %s ", separator);
         }
         printf(" %d ", va_arg(args, int));
     }
     va_end(args);
+    
     printf("\n");
 }
