bd6bb605cec2a175d0c00aacfbb429d551034e37
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-07-21 23:58:04 +0300

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,79 +1,21 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _printchar - print char type element from va_list
- * @list: va_list passed to function
+ * sum_them_all - return the sum of all its parameters
+ * @n: number of extra params
+ * Return: total sum
  */
-void _printchar(va_list list)
+int sum_them_all(const unsigned int n, ...)
 {
-	printf("%c", va_arg(list, int));
-}
+	unsigned int i;
+	va_list list;
+	int sum;
 
-/**
- * _printstr - print string element from va_list
- * @list: va_list passed to function
- */
-void _printstr(va_list list)
-{
-	char *s;
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-	s = va_arg(list, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
-}
+	va_end(list);
 
-/**
- * _printfloat - print float type element from va_list
- * @list: va_list passed to function
- */
-void _printfloat(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * _printint - print int type element from va_list
- * @list: va_list passed to function
- */
-void _printint(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_all - print anything passed if char, int, float, or string.
- * @format: string of formats to use and print
- */
-void print_all(const char * const format, ...)
-{
-	unsigned int i, j;
-	va_list args;
-	char *sep;
-
-	checker storage[] = {
-		{ "c", _printchar },
-		{ "f", _printfloat },
-		{ "s", _printstr },
-		{ "i", _printint }
-	};
-
-	i = 0;
-	sep = "";
-	va_start(args, format);
-	while (format != NULL && format[i / 4] != '\0')
-	{
-		j = i % 4;
-		if (storage[j].type[0] == format[i / 4])
-		{
-			printf("%s", sep);
-			storage[j].f(args);
-			sep = ", ";
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(args);
+	return (sum);
 }
