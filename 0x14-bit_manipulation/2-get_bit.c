46ef026211be870493c4302fa6979f5dedfd286b
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-08-04 02:22:42 +0300

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *get_bit - a function that uses the index
+ *variable to return the value of a bit
+ *@n: The decimal value passed
+ *@index: The particular bit being observed
+ *Return: the value of the bit at index or -1 if there was an error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n >> index;
+	if (index > 63)
+		return (-1);
+	n = n % 2;
+	return (n);
+}
